094d2daf73e51f120ff997352de1e6dbdaf51f7a
Arvin Agas <arvagas@gmail.com>
2019-12-11 17:47:44 -0500

    recipe batches complete

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    batch_amts = []
+
+    for i in recipe:
+        if i not in ingredients or recipe[i] > ingredients[i]:
+            return 0
+        else:
+            batch_amts.append(int(ingredients[i]/recipe[i]))
+        
+    return min(batch_amts)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test 
+    # your implementation with different inputs
+    recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+    ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
